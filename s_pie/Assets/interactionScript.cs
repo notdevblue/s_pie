a93bfee3cdaa95fb539b52667868d2184874aeb7
Seunghyeok <dltmdgur__@naver.com>
2021-01-13 19:08:08 +0900

    Revert "Revert "오브젝트 상호작용""
    This reverts commit 8b82b3b77a07ba61130b629eaeb00003d8ef3359.

---
+++ s_pie/Assets/interactionScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UIManager;
+
+
+public class interactionScript : MonoBehaviour
+{
+	//public enum Type { Customize, Mission, Emergency };
+	//public Type type;
+	GameObject Line; //테두리
+	public int curInteractionNum;
+
+
+	void Start()
+	{
+		//Line = transform.GetChild(0).gameObject;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.CompareTag("Player"))
+		{ 
+			//Line.SetActive(true);
+			UM.SetInteractionBtn(curInteractionNum, true);
+		}
+	}
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.CompareTag("Player"))
+		{
+			//Line.SetActive(false);
+			UM.SetInteractionBtn(0, false);
+		}
+	}
+
+}

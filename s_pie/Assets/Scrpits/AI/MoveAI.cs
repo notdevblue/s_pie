9421e3b7f629c8d246e04c1e28236de5846d45b3
devblue_ <wooyeophan@gmail.com>
2021-01-13 18:02:43 +0900

    닷트윈

--- s_pie/Assets/Scrpits/AI/MoveAI.cs
+++ s_pie/Assets/Scrpits/AI/MoveAI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 // 대충 하는 일 설명
 /*
@@ -31,22 +32,26 @@ public class MoveAI : MonoBehaviour
     private bool isYSame = false;
     private bool isXBigger = false;
     private bool isYBigger = false;
+
     /// <summary>
     /// 최적화가 이런거로 되나?
     /// </summary>
-    private bool calculated = false;
+    //private bool calculated = false;
+    
     #endregion
 
 
     void Start()
     {
+        // 뭐 빠트리고 실행시키면 귀찮으니
         if(!CheckDestinationStatus())
             UnityEditor.EditorApplication.isPlaying = false;
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        // TODO 우앱 : 인풋에서 턴으로 바꿔야 함
+        if(Input.GetKeyUp(KeyCode.Space))
         {
             Partrol();
         }
@@ -54,24 +59,53 @@ public class MoveAI : MonoBehaviour
 
     void Partrol()
     {
-        if (destination[des] == destination[des + 1])
+        if (isXSame && isYSame)
         {
             ++des;
             isXSame = false;
             isYSame = false;
-            calculated = false;
         }
 
-        switch(calculated)
+        PositionCalculate();
+        ToNextDestination();
+    }
+
+    void ToNextDestination()
+    {
+        switch (isXSame)
         {
+            #region 참
+            case true:
+                switch(isYBigger)
+                {
+                    case true:
+                        
+                        break;
+                    case false:
+
+                        break;
+                }
+                break;
+            #endregion
+
             case false:
-                PositionCalculate();
+                
+                break;
+        }
+        switch (isYSame)
+        {
+            case true:
+
+                break;
+            case false:
+
                 break;
         }
     }
 
     void PositionCalculate()
     {
+        // 뭔가 잘못 쓴거 같은데
         #region 만약 목적지 x좌표가 같거나 y좌표가 같다는 판단용
         if (!isXSame)
         {
@@ -96,21 +130,25 @@ public class MoveAI : MonoBehaviour
                 isYBigger = false;
         }
         #endregion
-        calculated = true;
+        //calculated = true;
     }
 
 
     /// <summary>
-    /// 목적지 검사
+    /// 목적지 검사.
+    /// !빌드할때는 제외해야 함! (안해도 문제는 없지만)
     /// </summary>
     /// <returns>문제 없을 시 true</returns>
     bool CheckDestinationStatus()
     {
+        // 순찰 지점이 2개 미만이면 순찰을 못 돔
         if (destination.Length < 2)
         {
             UnityEditor.EditorUtility.DisplayDialog("AI 목적지 수 오류", "AI 의 순찰 지점이 너무 적습니다.", "확인");
             return false;
         }
+        
+        // 배열 중 비어있는 요소가 있다면 순찰을 못 돔
         for (int findNull = 0; findNull != destination.Length; ++findNull)
         {
             if (destination[findNull] == null)
@@ -119,6 +157,14 @@ public class MoveAI : MonoBehaviour
                 return false;
             }
         }
+
+        #region AI 좌표와 첫 순찰 좌표가 다를 시 오류 발생 (꼭 필요한지 모르겠)
+        if (destination[0].transform.position != transform.position)
+        {
+            UnityEditor.EditorUtility.DisplayDialog("AI 위치 오류", "AI 의 첫 순찰 지점과 현제 AI 의 위치가 다릅니다.", "확인");
+            return false;
+        }
+        #endregion // 이게 꼭 필요한건가 궁금하기는 한데
         return true;
     }
 }

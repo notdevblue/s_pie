8338f78adc4a1cbe55930412f34027b9612a1f05
Firstund <yjw040608@gmail.com>
2021-01-21 02:05:23 +0900

    푸쉬

--- s_pie/Assets/Scrpits/Manager/GameManager.cs
+++ s_pie/Assets/Scrpits/Manager/GameManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Text;
 
 public class GameManager : MonoBehaviour
 {
-
     private static GameManager instance;
+    [SerializeField]
+    private ChallengeSaveData challengeSaveData;
 
     public static GameManager Instance
     {
@@ -23,13 +26,45 @@ public class GameManager : MonoBehaviour
             return instance;
         }
     }
+    private string jsonString = "";
+
+    private string filePath = "";
+
     private void Start()
     {
 
+    }
+    private void Awake()
+    {
+        challengeSaveData = new ChallengeSaveData();
+        filePath = string.Concat(Application.persistentDataPath, "/", "Save.txt");
+        ChallengeLoad();
+        Debug.Log("filePath : " + filePath);
     }
     private void Update()
     {
 
     }
-
+    public ChallengeSaveData GetChallengeSaveData()
+    {
+        return challengeSaveData;
+    }
+    public void ChallengeSave()
+    {
+        jsonString = JsonUtility.ToJson(challengeSaveData);
+        FileStream fs = new FileStream(filePath, FileMode.Create);
+        byte[] data = Encoding.UTF8.GetBytes(jsonString);
+        fs.Write(data, 0, data.Length);
+        fs.Close();
+        Debug.Log("JSON : " + jsonString);
+    }
+    public void ChallengeLoad()
+    {
+        FileStream fs = new FileStream(filePath, FileMode.Open);
+        byte[] data = new byte[fs.Length];
+        fs.Read(data, 0, data.Length);
+        fs.Close();
+        jsonString = Encoding.UTF8.GetString(data);
+        challengeSaveData = JsonUtility.FromJson<ChallengeSaveData>(jsonString);
+    }
 }

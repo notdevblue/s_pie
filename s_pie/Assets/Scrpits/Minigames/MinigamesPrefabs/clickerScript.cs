46eb9029b2597a9d479ec3dea57ae852e373796f
Firstund <yjw040608@gmail.com>
2021-01-21 18:13:44 +0900

    clickerScript

---
+++ s_pie/Assets/Scrpits/Minigames/MinigamesPrefabs/clickerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickerScript : MonoBehaviour
+{
+    private ClickerManager clickerManager = null;
+    private void Start()
+    {
+        clickerManager = FindObjectOfType<ClickerManager>();
+    }
+    private void Update()
+    {
+        DestroyClicker();
+    }
+    public void DestroyClicker()
+    {
+        if(clickerManager.GetGameIsClear())
+        {
+            // 미니게임 클리어시의 상황
+            Destroy(gameObject);
+        }
+        else if(clickerManager.GetGameIsOver())
+        {
+            // 미니게임 실패시의 상황
+            Destroy(gameObject);
+        }
+    }
+}

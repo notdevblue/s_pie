3b2eb5a252a0b0caec81aaee5c32a4379c464e83
sdy362 <seodye04@gmail.com>
2021-01-25 15:35:32 +0900

    턴 제한 추가. 플레이어 움직이는 버튼 이미지 변경

--- s_pie/Assets/Scrpits/Turn/TurnManager.cs
+++ s_pie/Assets/Scrpits/Turn/TurnManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnManager : MonoBehaviour
 {
@@ -10,18 +11,23 @@ public class TurnManager : MonoBehaviour
     private int turn = 0; //현재 턴이 몇인지 확인
     public bool playerTurn = true; //현재 플레이어 턴인지 확인
 
+    [SerializeField] private Text turnText; //턴UI
+    [Header("여기에 턴 제한 수를 넣으면 됨")]
+    [SerializeField] private int limitTurn; //턴 제한
+
     private void Awake()
     {
         moveAI   = FindObjectsOfType<MoveAI>();
         noticeAI = FindObjectsOfType<NoticeAI>();
+        turnText.text = limitTurn + "턴 남음"; //남은 턴 표시
     }
 
     public void EndPlayerTurn(int addTurn/*플레이어가 몇턴을 사용했는지 받아옴*/) //플레이어 턴
     {
         playerTurn = false;
         turn += addTurn;
-        Debug.Log(turn + "턴 지남");
         AITurn(addTurn); //AI턴으로 넘어감
+        TurnCheck(); //현재 턴이 제한된 턴을 넘었는지 확인
     }
 
     private void AITurn(int aITurn/*플레이어가 사용한 턴 만큼 AI가 이동함*/) //AI턴
@@ -41,5 +47,14 @@ public class TurnManager : MonoBehaviour
         #endregion
 
         playerTurn = true;
+    }
+
+    private void TurnCheck()
+    {
+        turnText.text = limitTurn - turn + "턴 남음"; //남은 턴 표시
+        if(turn == limitTurn)
+        {
+            //TODO: 게임 오버
+        }
     }
 }

e262a6e128a0578b3948a67568187b1586467043
sdy362 <seodye04@gmail.com>
2021-01-26 00:03:49 +0900

    오브젝트 확인 버튼

---
+++ s_pie/Assets/Scrpits/ObjectCheck/CanvasCameraChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasCameraChange : MonoBehaviour
+{
+    [SerializeField] private Transform playerPosition; //플레이어 위치
+    [SerializeField] private Canvas objectCheckCanvas; //오브젝트 확인 캔버스
+    [SerializeField] private Camera camera1F; //1층 카메라
+    [SerializeField] private Camera camera2F; //2층 카메라
+
+    public void PlayerFloorCheck()
+    {
+        if(playerPosition.localPosition.y < 6f)
+        {
+            //1층
+            if(objectCheckCanvas.worldCamera != camera1F)
+            {
+                objectCheckCanvas.worldCamera = camera1F;
+            }
+        }
+        else
+        {
+            //2층
+            if (objectCheckCanvas.worldCamera != camera2F)
+            {
+                objectCheckCanvas.worldCamera = camera2F;
+            }
+        }
+    }
+}
---
+++ s_pie/Assets/Scrpits/ObjectCheck/ObjectCheck.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCheck : MonoBehaviour
+{
+    [SerializeField] private GameObject objectCheck = null; //오브젝트 확인 캔버스 게임 오브젝트
+    [Header("여기에 상호작용 오브젝트를 모두 넣으면 됨")]
+    [SerializeField] private SpriteRenderer[] interactionObjects; //상호작용 오브젝트
+
+    public void ObjectCheckPointerDown() //버튼을 누르고 있을때
+    {
+        objectCheck.SetActive(true);
+        
+        for(int i = 0; i < interactionObjects.Length; i++) //상호작용 오브젝트를 오브젝트 확인 캔버스 앞으로 보냄
+        {
+            interactionObjects[i].sortingOrder = 1;
+        }
+    }
+
+    public void ObjectCheckPointerUp() //버튼에서 손을 놓았을때
+    {
+        objectCheck.SetActive(false);
+
+        for (int i = 0; i < interactionObjects.Length; i++) //상호작용 오브젝트를 오브젝트 확인 캔버스 뒤로 보냄
+        {
+            interactionObjects[i].sortingOrder = 0;
+        }
+    }
+}

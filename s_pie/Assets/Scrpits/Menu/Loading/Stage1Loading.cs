15f41234e1386321f78d8d026b5e319794964091
definitelynotdevblue <wooyeophan@gmail.com>
2021-01-30 11:09:22 +0900

    일시정지창에 아트 리소스를 적용

--- s_pie/Assets/Scrpits/Menu/Loading/Stage1Loading.cs
+++ s_pie/Assets/Scrpits/Menu/Loading/Stage1Loading.cs
@@ -17,8 +17,8 @@ public class Stage1Loading : MonoBehaviour
     private IEnumerator LoadAsyncOperation()
     {
         AsyncOperation stage = SceneManager.LoadSceneAsync("Stage1");
-
-        while(stage.progress < 1)
+        
+        while (stage.progress < 1)
         {
             prog.value = stage.progress;
             yield return new WaitForEndOfFrame();

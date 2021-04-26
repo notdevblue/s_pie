c9987e96d23bdfcc8e2a8d406568ffe48d6264af
Han not working these days <wooyeophan@gmail.com>
2021-04-26 22:42:10 +0900

    비앙카 스테이지의 기틀을 닦아가고잇는 우앱씨
    * TTR 안하고 하고잇슴 후후후후후

---
+++ s_pie/Assets/Scrpits/_HardMode/Player/PlayerMouseMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseMove : MonoBehaviour
+{
+    private Vector3 mousePos = Vector3.zero;
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0.0f;
+        }
+        transform.position = mousePos; // 귀찬아서 일케했지만 시간단위로 움직여야함
+    }
+}

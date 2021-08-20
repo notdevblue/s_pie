cf60f408b2ffbfe244112f65aadeda06e90418a8
ubuntuHan <wooyeophan@gmail.com>
2021-08-20 23:08:43 +0900

    폴더 이름 일관성있게 수정함

---
+++ s_pie/Assets/001_Scrpits/TestScripts/LightEffectTestScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEffectTestScript : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
